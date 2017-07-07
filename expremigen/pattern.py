03eb1da212e4fc3d1ae56a3b1e9bf89bae238dbb
stefaan himpe <stefaan.himpe@gmail.com>
2017-07-08 00:00:28 +0200

    more generic place to define __repr__

--- expremigen/pattern.py
+++ expremigen/pattern.py
@@ -5,10 +5,5 @@ class Pattern(metaclass=abc.ABCMeta):
     def __str__(self):
         return "{0}".format(self.__class__.__name__)
 
-
-def flatten(l):
-    for el in l:
-        if isinstance(el, Pattern):
-            yield from flatten(el)
-        else:
-            yield el
+    def __repr__(self):
+        return self.__str__()
