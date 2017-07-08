8ad5717caf4e03b369f1d286abe152acb4b3a8b9
stefaan himpe <stefaan.himpe@gmail.com>
2017-07-08 13:35:31 +0200

    add a pwhite pattern

---
+++ expremigen/patterns/pwhite.py
@@ -0,0 +1,22 @@
+import random
+import sys
+
+from expremigen.patterns.pattern import Pattern
+
+
+class Pwhite(Pattern):
+    def __init__(self, lo=0, hi=1, repeats=sys.maxsize):
+        super().__init__()
+        self.lo = lo
+        self.hi = hi
+        self.repeats = repeats
+
+    def __str__(self):
+        return "{0}({1}, {2}, {3})".format(self.__class__.__name__, self.lo, self.hi, self.repeats)
+
+    def __iter__(self):
+        for _ in range(self.repeats):
+            yield random.uniform(self.lo, self.hi)
+
+    def __repr__(self):
+        return self.__str__()
---
+++ tests/testpwhite.py
@@ -0,0 +1,21 @@
+import unittest
+
+from expremigen.patterns.pwhite import Pwhite
+
+
+class TestPwhite(unittest.TestCase):
+    def test_normal(self):
+        a = [i for i in Pwhite(4, 5, 10)]
+        print(a)
+        self.assertEqual(len(a), 10)
+
+    def test_empty(self):
+        a = [i for i in Pwhite(4, 5, 0)]
+        self.assertEqual(a, [])
+
+    def test_repr(self):
+        self.assertEqual("{0}".format(Pwhite(0, 1.5, 4)), "Pwhite(0, 1.5, 4)")
+
+
+if __name__ == '__main__':
+    unittest.main()
