609f5227ebe5b3224c9fc27848256fa55e826592
stefaan himpe <stefaan.himpe@gmail.com>
2017-07-10 00:01:16 +0200

    add missing Pchord representation test

--- tests/testpchord.py
+++ tests/testpchord.py
@@ -8,6 +8,8 @@ class TestPchord(unittest.TestCase):
         a = [i for i in Pchord(["a4", "c#5", "e5"])]
         self.assertEqual(a[0].notes, ["a4", "c#5", "e5"])
 
+    def test_repr(self):
+        self.assertEqual("{0}".format(Pchord(["c3", "e3", "g3"])), "Pchord(['c3', 'e3', 'g3'])")
 
 if __name__ == '__main__':
     unittest.main()
