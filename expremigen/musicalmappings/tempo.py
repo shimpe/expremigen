68ec6e2b75944c20b09ccd7b48c3adad41dc9045
stefaan himpe <stefaan.himpe@gmail.com>
2017-07-11 22:17:36 +0200

    class to hold some popular tempo indications

---
+++ expremigen/musicalmappings/tempo.py
@@ -0,0 +1,25 @@
+class Tempo:
+    """
+    convert some italian tempo indications to bpm
+    (in reality the names indicate a range of tempi;
+     I just selected one at random)
+    """
+    larghissimo = 20
+    grave = 40
+    lento = 44
+    largho = 50
+    larghetto = 60
+    adagissimo = 63
+    adagio = 69
+    adagietto = 72
+    andante = 80
+    andantino = 86
+    moderato = 112
+    allegretto = 120
+    allegro = 132
+    vivace = 144
+    allegro_vivace = 152
+    vivacissimo = 160
+    allegro_assai = 176
+    presto = 184
+    prestissimo = 208
