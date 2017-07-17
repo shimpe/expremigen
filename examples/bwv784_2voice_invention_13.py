14323b353a055aa784262a3aa23e5018cf4b7787
stefaan himpe <stefaan.himpe@gmail.com>
2017-07-17 15:28:42 +0200

    small simplification of mispel based bach example

--- examples/bwv784_2voice_invention_13.py
+++ examples/bwv784_2voice_invention_13.py
@@ -6,8 +6,8 @@ outputfile = "output/bwv784_2voice_invention_13.mid"
 def create_bach():
     m = Mispel()
     m.parse(r"""
-    with track 0 channel 0 time 0:
-        r_16\pdur[legato]\vol{f} e4 a c5 b4 e b d5 c_8\pdur[staccato]\vol{ff} e g#4 e5 
+    with track 0:
+        r_16\pdur[legato]\vol{f} e4 a c5 b4 e b d5 c_8\pdur[staccato]\vol{ff} e g#4 e5
         a4_16\vol{f} e4_16\pdur[legato] a c5 b4 e b d5 c_8\vol{ff}\pdur[staccato] a4 r_4\vol{f}
         r_16 e5\vol{p}\pdur[legato] c e a4 c5 e4 g f_8\vol{ff}\pdur[staccato] a\vol{f} d5 f_8.\vol{p}
         d_16\pdur[legato] b4 d5 g4 b d f e_8\vol{ff}\pdur[staccato] g\vol{f} c5 e_8.\vol{p}
@@ -15,7 +15,7 @@ def create_bach():
         e_8\vol{ff}\pdur[staccato] c5_8.\vol{p}\pdur[legato] 
         a4_16 f a d_8\vol{ff}\pdur[staccato] b_8 c5_8 r_8 r_4
         
-    with track 1 channel 0 time 0:
+    with track 1:
         a2_8\vol{mf}\pdur[staccato] a3_4\pdur[legato]\vol{f} g#_8\vol{mf} a_16\pdur[staccato]\vol{f} e3\pdur[legato] a c4 b3 e b d4 
         c_8\pdur[staccato]\vol{ff} a3 g# e a_16\vol{f} e\pdur[legato] a c4 b3 e b d4 
         c_8\pdur[staccato]\vol{ff} a3 c4 a3 
