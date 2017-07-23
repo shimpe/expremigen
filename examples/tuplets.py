7a7e7bbea29bfea8f40e9b26cdfe18cd478b5d43
stefaan himpe <stefaan.himpe@gmail.com>
2017-07-23 17:40:45 +0200

    clean up the tuplets example to use the simpler multiplier syntax

--- examples/tuplets.py
+++ examples/tuplets.py
@@ -1,18 +1,18 @@
 from expremigen.mispel.mispel import Mispel
 from expremigen.io.pat2midi import Pat2Midi
 
-outputfile = "output/chopin.mid"
+outputfile = "output/tuplets.mid"
 
 def make_midi():
     m = Mispel()
     m.parse(r"""
     with track 0 channel 0:
          r_1*4 |
-         r_16 g#4\vol{p} a g# fx g# c#5 e\vol{mf} d# c# d# c# b#4 c# e g#\vol{p} |
-         r_16 g#4\vol{p} a g# fx g# c#5 e\vol{mf} d# c# d# c# b#4 c# e g#\vol{p} |
+         r_16 g#4\vol{p} a g# fx g# c#5 e\vol{mf} d# c# d# c# b#4 c#5 e g#\vol{p} |
+         r_16 g#4\vol{p} a g# fx g# c#5 e\vol{mf} d# c# d# c# b#4 c#5 e g#\vol{p} |
         
     with track 1 channel 1:
-        <g#2_1*2/1\vol{ff} g#3> | 
+        <g#2_1*2\vol{ff} g#3> | 
         <c#2_8*2/3\vol{f} c#3> g#3 c#4 e c# g#3 c# g# c#4 e c# g#3 |
         c# g# c#4 e c# g#3 c# g# c#4 e c# g#3\vol{p}  |
         c# g# c#4 e c# g#3 e g# c#4 e c# g#3 |
