b00bad13ffa3f40eceb1a816fbc8167993258046
stefaan himpe <stefaan.himpe@gmail.com>
2018-12-08 09:02:06 +0100

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='expremigen',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=['build', 'dist', 'tests']),
     install_requires=['vectortween', 'MIDIUtil', 'textX'],
     url='https://github.com/shimpe/expremigen',
